29b7fdd36a5e8e714bb9a2bd7f4bdada5d0ffab7
EgorovM <egorov_michil@mail.ru>
2018-12-01 23:02:06 +0900

    bot

---
+++ telegram bot/bot.py
@@ -0,0 +1,21 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+token='741580126:AAHhBb0Tju-JPAlcc5BdoJ4JMcEHEzEDh5o'
+updater = Updater(token)
+dispatcher = updater.dispatcher
+
+def startCommand(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text='Привет, давай пообщаемся?')
+    
+def textMessage(bot, update):
+    response = 'Получил Ваше сообщение: ' + update.message.text
+    bot.send_message(chat_id=update.message.chat_id, text=response)
+
+start_command_handler = CommandHandler('start', startCommand)
+text_message_handler = MessageHandler(Filters.text, textMessage)
+
+dispatcher.add_handler(start_command_handler)
+dispatcher.add_handler(text_message_handler)
+
+updater.start_polling(clean=True)
+updater.idle()
